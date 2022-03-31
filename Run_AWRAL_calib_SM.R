dca0e835cca7933a616004496b12c4344e81a7a8
DJackCai <86879189+DJackCai@users.noreply.github.com>
2022-03-31 21:57:50 +1100

    Update and rename Run_AWRAL_calib.R to Run_AWRAL_calib_SM.R

--- Run_AWRAL_calib_SM.R
+++ Run_AWRAL_calib_SM.R
@@ -4,56 +4,54 @@
 # the length and the components of "pars" will differ. 
 # make sure all the data read in are in the matrix format, using the as.matrix() command. 
 
-Run_AWRAL_calib_GB <- function(pars,all_forcing,STATIC_PAR,N_GRID=1)  {
+Run_AWRAL_calib_SM <- function(pars,all_forcing,STATIC_PAR,N_GRID=1)  {
   
   ## all_forcing: full time series of each forcing variable for the site 
-  ## STATIC_PAR: the static parameters including ftree, 3 SzFC and mean potential evapotranspiration
-  
-  
-  II = rbind(1,1)
+  ## states: vector of initial states (passed unchanged)
+  II = rbind(N_GRID,N_GRID)
   
   Init_PARAMS <- PARAMS_set(II,STATIC_PAR = STATIC_PAR)
-   
+  
   #####  Update the initial parameters by the pars to be optimised  #####
-
-    ###### Updated: must be the in the Fhru format, there are no "Ud01" etc in the parameter set 
-  Init_PARAMS$Ud0 = II*c(pars[1],0)
-  Init_PARAMS$FsoilEmax = II * c(pars[2],0.5)  
-  Init_PARAMS$S_sls = II * c(pars[3],0.1)
-  Init_PARAMS$ER_frac_ref = II*c(pars[4],0.05)
-  Init_PARAMS$FdrainFC = II * c(pars[5],0.029) 
-  Init_PARAMS$beta <- II * c(pars[6],4.5)
-  Init_PARAMS$LAImax <- II * c(pars[7],8)
+  
+  ###### in the Fhru format
+  
+  Init_PARAMS$Vc <- II*c(pars[1],0.65)
+  Init_PARAMS$Gfrac_max <- II*c(0.3,pars[2])
+  
+  Init_PARAMS$Ud0 = II*c(pars[3],0)
+  Init_PARAMS$FsoilEmax = II * c(pars[4],pars[5])
+  Init_PARAMS$S_sls = II * c(pars[6],pars[7])
+  Init_PARAMS$ER_frac_ref = II*c(pars[8],0.05)
+  Init_PARAMS$FdrainFC = II * c(pars[9],pars[10]) 
+  Init_PARAMS$beta <- II * c(pars[11],pars[12])
  
   PARAMS <- Init_PARAMS  # updated parameter
   
   ## Extract forcing data for ALL TIME
-  
   PG <- all_forcing$PG
   RG <- all_forcing$RG
   TA <- all_forcing$TA
   PE <- all_forcing$PE
   U2 <- all_forcing$U2
   PAIR = rep(97500.00,1)
-
-  ## Forcing for one time step
-  FORCING = empty_forcing()  # external function
   
-  ######  Updated spatialised parameters (for those identified as insensitive, continue using the regression relationship adopted in the report)
+  ## Forcing for one time step
+  FORCING = empty_forcing()
+  
+  ###  Updated spatialised parameters (for those insensitive)
   meanP   = mean(PG)
   meanPET = STATIC_PAR$meanPET
   humidity = meanP / meanPET
   
-  Sgref= pmax(0.0,  8.15 * meanP^2.34)
-  FdrainFC = pmax(pmin(0.0685*rbind(humidity,humidity)^3.179,0.3),0.005)
   K_gw   = 0.047*(humidity^(-0.0508))
   K_rout = 0.141*meanPET + 0.284
+  Sgref = pmax(0.0,  8.15 * meanP^2.34)
   
   ##  BACK-updating 
+  PARAMS$Sgref = Sgref
   PARAMS$K_gw    = K_gw
   PARAMS$K_rout  = K_rout
-  PARAMS$Sgref    = Sgref
-  PARAMS$FdrainFC = II * c(pars[7],FdrainFC[2,])    # replace FdrainFC2 when it's insensitive 
   
   ### update states 
   STATES = list(
@@ -66,13 +64,13 @@ Run_AWRAL_calib_GB <- function(pars,all_forcing,STATIC_PAR,N_GRID=1)  {
   
   ## Storage
   
-  NL <- length(PG)  # define number of time steps 
+  NL <- length(PG)  # define number of time steps; here PG is a vector
   S0mean     = matrix(NA,NL,1)
- # QTOT       = matrix(NA,NL,1)
+  QTOT       = matrix(NA,NL,1)
+  LAI = matrix(NA,NL,1)
   
   ######## Time stepping  ######
   for (k in 1:(NL-1)) {
-  
     # loop over each time step's forcing
     FORCING$Pg   = rbind(PG[k],PG[k])
     FORCING$Rg   = rbind(RG[k],RG[k])
@@ -82,12 +80,10 @@ Run_AWRAL_calib_GB <- function(pars,all_forcing,STATIC_PAR,N_GRID=1)  {
     # FORCING$u2   = U2
     FORCING$u2   = rbind(U2[k],U2[k])
     
-    # Run for single time step by calling external timestepping function
+    # Run for single time step
     OUTPUTS = AWRAL_TimeStep(forcing =FORCING,state = STATES,par = PARAMS )
-
-    #### Update the model states, to be used to propogate next time step 
-    ## still a N*2 matrix accounting for each fhru
     
+    # Assign the new state - to be used as forecast at next
     STATES$S0    = OUTPUTS$S0
     STATES$Ss    = OUTPUTS$Ss
     STATES$Sd    = OUTPUTS$Sd
@@ -96,7 +92,8 @@ Run_AWRAL_calib_GB <- function(pars,all_forcing,STATIC_PAR,N_GRID=1)  {
     STATES$Mleaf = OUTPUTS$Mleaf
     
     ## Store the key states and fluxes, same as length of forcings
-    S0mean[k,] = c(1,1) %*% (STATES$S0 * PARAMS$Fhru)  # combining with Fhru to give one single, grid-level estimate
-    QTOT[k,]   = OUTPUTS$Qtot        # versatile, to be used for streamflow calibration
+    S0mean[k,] = c(1,1) %*% (STATES$S0 * PARAMS$Fhru)
+    QTOT[k,]   = OUTPUTS$Qtot
+    LAI[k,] = OUTPUTS$LAI
   }
-  return(list(S0mean=S0mean,QTOT=QTOT)) }    
+  return(list(S0mean=S0mean,QTOT=QTOT,LAI=LAI)) }
